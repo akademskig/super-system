7173f6ee84139c616d9ed72aa78ce320dfefd8cc
Marta Susek <m.susek@live.com>
2021-12-22 12:26:20 +0100

    frontend/games: delete game2

--- frontend/src/routes/MainRoutes.tsx
+++ frontend/src/routes/MainRoutes.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import Game1 from '../components/modules/Game1'
-import Game2 from '../components/modules/Game2'
 import AuthPage from '../components/pages/AuthPage'
 import MainPage from '../components/pages/MainPage'
 
@@ -10,7 +9,6 @@ const MainRoutes = () => {
       <Route path="/*" element={<MainPage />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/games/game1" element={<Game1 />} />
-      <Route path="/games/game2" element={<Game2 />} />
     </Routes>
   )
 }
