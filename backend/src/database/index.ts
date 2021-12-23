78b74df6e38cb14101aebed99c83a40a77ee749b
Marta Susek <m.susek@live.com>
2021-12-23 13:16:31 +0100

    feat/backend: remove unused entites

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,10 +1,6 @@
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import User from './entity/user.entity';
 import VerificationToken from './entity/verificationToken.entity';
-import Event from './entity/event.entity';
-import Location from './entity/location.entity';
-import Instrument from './entity/instrument.entity';
-import MidiFile from './entity/midiFile.entity';
 
 export const dbConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -13,7 +9,7 @@ export const dbConfig: TypeOrmModuleOptions = {
   username: 'dbadmin',
   password: 'dbadmin#123',
   database: 'dbex',
-  entities: [User, VerificationToken, Event, Location, Instrument, MidiFile],
+  entities: [User, VerificationToken],
   synchronize: true,
 };
 const typeOrmModule = TypeOrmModule.forRoot(dbConfig);
